955cd05a2f07da5d6672682dff4fabad818a7208
mmanoharan1 <madhanarakesh.manoharan@vvdntech.in>
2026-06-06 18:10:41 +0530

    updated c code 2

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
-
+int m = 0;
 int main() {
     printf("Hello, World!\n");
-    printf("-----exit----");
+    printf("-----exit----\n");
+    if(m == 0)
+	   {
+		   printf("it was wrking");
+	   }
+    else
+    {
+	    printf("Not working");
+    }
     return 0;
 }
